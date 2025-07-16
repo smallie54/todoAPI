852a6aece27ae4213c0988316e7d82656ccef82c
Praise <adefagbayipraise@gmail.com>
2025-07-16 16:35:18 +0100

    setup cors

--- src/server.js
+++ src/server.js
@@ -1,27 +1,35 @@
-import express from "express"
-import dotenv from "dotenv"
-import chalk from "chalk"
-import { connectDB } from "./config/db.js"
-import routes from "./routes/index.js"
+import express from "express";
+import dotenv from "dotenv";
+import chalk from "chalk";
+import { connectDB } from "./config/db.js";
+import routes from "./routes/index.js";
+import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
 //creating an app
-const app = express()
-const PORT = process.env.PORT
+const app = express();
+const PORT = process.env.PORT;
 
+app.use(
+  cors({
+    origin: [
+      process.env.CLIENT_PRODUCTION_URL,
+      "https://todoapi-0ep7.onrender.com",
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 //creating middleware
-app.use(express.json())
+app.use(express.json());
 
-
-app.use('/', routes)
-
-
+app.use("/", routes);
 
 connectDB().then(() => {
-    app.listen(PORT, ()=>{
-        console.info(chalk.blue(`SERVER is LIVE http://localhost:${PORT}`))
-    })
-})
- 
+  app.listen(PORT, () => {
+    console.info(chalk.blue(`SERVER is LIVE http://localhost:${PORT}`));
+  });
+});
